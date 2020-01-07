557d0980704fa11d8797d402ddf52834cb06dc41
unknown <khaleedabuhawwas7@gmail.com>
2020-01-07 16:39:24 +0200

    first commit

---
+++ js/plaugin.js
@@ -0,0 +1,34 @@
+var goUpButton = document.getElementById('contactMe');
+var name = document.getElementById('name');
+var Email = document.getElementById('Email');
+var phone = document.getElementById('phone');
+
+
+
+//Validation
+
+var Button=document.getElementById('click');
+
+Button.onclick =function(){
+
+    e.preventDefault();
+
+    if(name === ''){
+        alert("Plece Enter Your Name")
+    }else if({
+
+
+
+    }
+
+
+
+}
+
+
+
+//Go Up Button
+goUpButton.onclick = function () {
+
+    scrollTo(0, 0);
+}
